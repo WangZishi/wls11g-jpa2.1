e8fac332b90f04a1f30c8a7fb8af149607fc18b2
Wang Zishi <ynh.2@outlook.com>
2016-03-09 11:32:37 +0800

    Update dockerfile and script

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,12 @@
-FROM riddick/weblogic11g_asms
+FROM riddick/weblogic11g_asms:last
 MAINTAINER Wang Zishi "ynh.2@outlook.com"
+
+WORKDIR /opt/bea/wls1036
+COPY jar/hibernate-jpa-2.1-api-1.0.0.Final.jar ./modules
+
+WORKDIR /opt/bea/wls1036/user_projects/domains/asms_domain
+RUN rm startWeblogic.sh
+COPY startWeblogic.sh ./
+
+EXPOSE 7001
+CMD ["/bin/bash ./startWeblogic.sh"]
---
+++ startWeblogic.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# WARNING: This file is created by the Configuration Wizard.
+# Any changes to this script may be lost when adding extensions to this configuration.
+
+# Hack JPA begin
+echo Hack JPA begin
+
+WLS_MODULES="/opt/bea/wls1036/modules"
+export WLS_MODULES
+
+PRE_CLASSPATH="${WLS_MODULES}\hibernate-jpa-2.1-api-1.0.0.Final.jar"
+export PRE_CLASSPATH
+
+echo PRE_CLASSPATH=${PRE_CLASSPATH}
+
+echo Hack JPA End
+# Hack JPA END
+
+WLS_USER="weblogic"
+
+export WLS_USER
+
+WLS_PW="weblogic1"
+
+export WLS_PW
+
+DOMAIN_HOME="/opt/bea/wls1036/user_projects/domains/asms_domain"
+
+${DOMAIN_HOME}/bin/startWebLogic.sh $*
